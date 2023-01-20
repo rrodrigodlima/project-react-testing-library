a08a878b92f8334940366208c005f0374ba568f7
rrodrigodlima <rdrigo019@gmail.com>
2023-01-20 18:23:23 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('Teste se a aplicação é redirecionada para a página Not Found ao entrar em uma URL desconhecida.', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const INVALID_URL = '/404';
+    act(() => {
+      history.push(INVALID_URL);
+    });
+
+    const notFoundTitle = screen.getByRole(
+      'heading',
+      { name: 'Page requested not found' },
+    );
+
+    expect(notFoundTitle).toBeInTheDocument();
+
+    const notFoundImg = screen.getByAltText('Pikachu crying because the page requested was not found');
+    expect(notFoundImg.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
